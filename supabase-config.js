df52f163aa5d6c93fa2c190ea562a40d03ac0ddb
Task-Window <debibanditarout@gmail.com>
2026-08-23 16:22:16 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://vtznzbzarzjjruhbquce.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_0buqcyB763Nj8-tGV2Uaag_a8AnG6Z4";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
